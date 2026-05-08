4c78d9731b974270fa429bc6f0d1cb4043ca3c43
jo261181 <jlavanderburgt@gmail.com>
2026-05-08 18:05:59 +0200

    Remove merge conflict markers and clean up unused imports in Footer, Heading, AboutUs, and EventPage components

--- public/images/Heading.jsx
+++ public/images/Heading.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <Box 
   position="sticky" 
   top="0" 
@@ -7,14 +6,4 @@
   boxShadow="sm"
 >
   <Heading>Sticky Heading</Heading>
-=======
-<Box 
-  position="sticky" 
-  top="0" 
-  bg="white" 
-  zIndex="sticky" // Uses Chakra's zIndex.sticky (1100)
-  boxShadow="sm"
->
-  <Heading>Sticky Heading</Heading>
->>>>>>> 3637cb57b823052f655bcd3b3789c641b297ff48
 </Box>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Flex, Button, Heading, Image } from "@chakra-ui/react";
 import SimpleModal from "../components/ui/modal";
 import { useState } from "react";
@@ -25,31 +24,3 @@ export const Navigation = ({ categories, addEvent }) => {
     </>
   );
 };
-=======
-import { Flex, Button, Heading, Image } from "@chakra-ui/react";
-import SimpleModal from "../components/ui/modal";
-import { useState } from "react";
-import EventForm from "../components/ui/EventForm";
-
-export const Navigation = ({ categories, addEvent }) => {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  return (
-    <>
-      <Flex gap={8} align="center" p={4}>
-        <Heading>
-          <Flex align="center"></Flex>
-        </Heading>
-      </Flex>
-
-      <SimpleModal
-        open={modalOpen}
-        onClose={() => setModalOpen(false)}
-        title="Create a new event"
-      >
-        <EventForm categories={categories} addEvent={addEvent} />
-      </SimpleModal>
-    </>
-  );
-};
->>>>>>> 3637cb57b823052f655bcd3b3789c641b297ff48
