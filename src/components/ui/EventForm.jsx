c8c4a40deea463d57ad945cb4dfebc9554fe1adb
jo261181 <jlavanderburgt@gmail.com>
2026-05-08 17:03:45 +0200

    Add files via upload
    exam

---
+++ src/components/ui/EventForm.jsx
@@ -0,0 +1,90 @@
+import {
+  Button,
+  Card,
+  Field,
+  Input,
+  Stack,
+  Textarea,
+} from "@chakra-ui/react";
+
+export default function EventForm({ cancel, initialEvent, onSubmit }) {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const values = Object.fromEntries(formData.entries());
+
+    onSubmit(values);
+  }
+
+  return (
+    <Card.Root maxW="sm" as="form" onSubmit={handleSubmit}>
+      <Card.Header>
+        <Card.Title>
+          {initialEvent ? "Edit Event" : "Create Event"}
+        </Card.Title>
+        <Card.Description>
+          {initialEvent
+            ? "Update the event details below"
+            : "Fill in the form below to create an event"}
+        </Card.Description>
+      </Card.Header>
+
+      <Card.Body>
+        <Stack gap="4" w="full">
+
+          <Field.Root>
+            <Field.Label>Event Name</Field.Label>
+            <Input
+              name="title"
+              required
+              defaultValue={initialEvent?.title}
+            />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Event Description</Field.Label>
+            <Textarea
+              name="description"
+              required
+              defaultValue={initialEvent?.description}
+            />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Startdate and Time</Field.Label>
+            <Input
+              type="datetime-local"
+              name="startTime"
+              required
+              defaultValue={initialEvent?.startTime}
+            />
+          </Field.Root>
+
+          <Field.Root>
+            <Field.Label>Enddate and Time</Field.Label>
+            <Input
+              type="datetime-local"
+              name="endTime"
+              required
+              defaultValue={initialEvent?.endTime}
+            />
+          </Field.Root>
+
+        </Stack>
+      </Card.Body>
+
+      <Card.Footer justifyContent="flex-end" gap={3}>
+        <Button variant="ghost" onClick={cancel}>
+          Cancel
+        </Button>
+
+        <Button variant="solid" type="submit" colorScheme="blue" width="inherit">
+          {initialEvent ? "Save changes" : "Create Event"}
+        </Button>
+      </Card.Footer>
+    </Card.Root>
+  );
+}
+
---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,24 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Footer() {
+  return (
+    <Box
+      bg="gray.100"
+      opacity="50%"
+      py={4}
+      mt={10}
+      textAlign="center"
+      
+    //   boxShadow="inner"
+    >
+      <Text 
+        fontSize="sm"
+        color="black"
+        fontWeight="semibold"
+
+      >
+        © {new Date().getFullYear()} PixelBloom Drift. All rights reserved.
+      </Text>
+    </Box>
+  );
+}
